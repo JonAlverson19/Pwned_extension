12139f8ca71e7154319f6c1e1ef0ea7999f2d516
JonAlverson19 <66183402+JonAlverson19@users.noreply.github.com>
2020-09-01 16:51:37 -0500

    Memory
    Implemented memory so that visiting a site where a pwned password was entered last will always show unlocked icon

--- background.js
+++ background.js
@@ -1,21 +1,40 @@
 //add listener for messages from content.js
 chrome.runtime.onMessage.addListener(
-  function(request, sender, sendResponse) {
-	//change logo and popup to specified only on this tab and while on this site
-    if(request.imgPath != "") { chrome.browserAction.setIcon({path: {"48": request.imgPath}, tabId: sender.tab.id}); }
-	if(request.popupPath != "") { chrome.browserAction.setPopup({popup: request.popupPath, tabId: sender.tab.id}); }
-	
-	//save url that used pwned password
-	if(request.url != ""){
-	  if (request.url.split('.')[0] == "add") { urls.add(request.url.split('.')[1]); }
-	  else{
-	  	  if(urls.has(request.url.split('.')[1])) { urls.delete(request.url.split('.')[1]); }
-	  }
+	function (request, sender, sendResponse){
+		//change logo and popup to specified only on this tab
+		if(request.imgPath != ""){ chrome.browserAction.setIcon({path: {"48": request.imgPath}, tabId: sender.tab.id}); }
+		if(request.popupPath != ""){ chrome.browserAction.setPopup({popup: request.popupPath, tabId: sender.tab.id}); }
+		//save url that used pwned password
+		if(request.url != ""){
+			//update urls before making changes
+			chrome.storage.local.get('urlList', function(item){
+				urls = item.urlList;
+			});
+			//console.log("1",urls);
+			if(request.url.split(':')[0] == "add"){ 
+				if(!urls.includes(request.url.split(':')[1].trim())){
+					urls.push(request.url.split(':')[1].trim());
+				}
+			}
+			else{
+				for( var i = 0; i < urls.length; i++){ 
+					if ( urls[i] === request.url.split(':')[1].trim()){ 
+						urls.splice(i, 1); i--; 
+					}
+				}
+			}
+			//update urls with new info
+			chrome.storage.local.set({'urlList': urls});
+		}
 	}
-	//console.log("set: " + urls.values())
-  }
 );
 
-var urls = new Set();
+var urls = new Array();
 
-//need to iterate over the set to determine if page should have unlocked logo and html
+//save urls to storage if it doesn't exist
+chrome.storage.local.get('urlList', function(item){
+	if (typeof item.urlList === "undefined"){
+		chrome.storage.local.set({urlList: urls});
+		console.log("setting urlList");
+	}
+});
